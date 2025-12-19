67e202221e9996fdb21c6c91346724adb008df8f
sachinthavidu <padsviduranga@students.nsbm.ac.lk>
2025-12-19 17:26:13 +0530

    base setup initializing

---
+++ src/main/java/com/example/uniCircle/UniCircleApplication.java
@@ -0,0 +1,13 @@
+package com.example.uniCircle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniCircleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniCircleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/uniCircle/UniCircleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uniCircle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniCircleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
