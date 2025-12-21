8c898fd960ccf229679bd76b848abfb8cd862c44
sachinthavidu <padsviduranga@students.nsbm.ac.lk>
2025-12-21 22:29:13 +0530

    refactor: improve Register

--- src/main/java/com/example/uniCircle/DTO/RegisterRequest.java
+++ src/main/java/com/example/uniCircle/DTO/RegisterRequest.java
@@ -4,9 +4,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-import jdk.jfr.DataAmount;
 
-@DataAmount
 public class RegisterRequest {
     @NotBlank(message = "required full name ")
     @Size(min = 3, max = 30,message = "full name should be between 3 and " +
@@ -24,4 +22,65 @@ public class RegisterRequest {
             message ="password should contain at leat one uppercase and one lowercase" +
                     "letter" )
     private String password;
+
+    public RegisterRequest(){
+
+    }
+    public RegisterRequest(String fullName,String email,String password){
+        this.fullName=fullName;
+        this.email=email;
+        this.password=password;
+    }
+    public String getFullName(){
+        return fullName;
+    }
+    public void setFullName(String fullName){
+        this.fullName=fullName;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public String getPassword(){
+        return password=password;
+    }
+    public void setPassword(String password){
+        this.password=password;
+    }
+
+    @Override
+    public String toString() {
+        return "RegisterRequest{" +
+                "fullName='" + fullName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+    @Override
+    public boolean equals(Object object){
+        if (this==object) return true;
+        if (object==null || getClass() !=object.getClass())
+            return false;
+
+        RegisterRequest request=(RegisterRequest) object;
+
+        if (!fullName.equals(request.fullName))
+            return false;
+        if (!email.equals(request.email))
+            return false;
+
+        return password.equals(request.password);
+    }
+    @Override
+    public int hashCode(){
+        int result=fullName.hashCode();
+        result=31*result+email.hashCode();
+        result=31*result+password.hashCode();
+
+        return result;
+    }
+
+
 }
