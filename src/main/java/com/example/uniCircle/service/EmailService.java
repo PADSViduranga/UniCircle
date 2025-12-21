4ba613214b0820fd1b9e98c90ee900d71dd58244
sachinthavidu <padsviduranga@students.nsbm.ac.lk>
2025-12-21 16:58:49 +0530

    update email service for more verifications

--- src/main/java/com/example/uniCircle/service/EmailService.java
+++ src/main/java/com/example/uniCircle/service/EmailService.java
@@ -23,6 +23,62 @@ public class EmailService {
             logger.error("Failed to send verification email to: {}", email, e);
             throw new RuntimeException("Failed to send verification email", e);
         }
+    }
+    public void sendPasswordResetEmail(String email, String resetToken){
+        try {
+            String resetLink="http://localhost:8080/api/auth/reset-password?token="+resetToken;
 
+            logger.info("----password reset email----");
+            logger.info("To: {}",email);
+            logger.info("subject: password reset email request");
+            logger.info("Reset Link: {}",resetLink);
+            logger.info("----------------------");
+        }
+        catch (Exception e){
+            logger.error("Failed to send password reset email to: {}",email,e);
+            throw new RuntimeException("failed to send password set email",e);
+        }
+    }
+    public void sendWelcomeEmail(String email,String fullName){
+        try {
+            logger.info("---Welcome Email");
+            logger.info("To : {}",email);
+            logger.info("subject: welcome to Unicircle");
+            logger.info("hello {}, Welcome to uni circle",email);
+            logger.info("---------------------------");
+        }
+        catch (Exception e){
+            logger.error("welcom email sending failed to: {}",email,e);
+            throw new RuntimeException("Failed ");
+        }
+    }
+
+    public void sendAccountLockedEmail(String email,String reason){
+        try {
+            logger.info("----Account locked email----");
+            logger.info("To: {}",email);
+            logger.info("subject: your account has been locked");
+            logger.info("Reason: {}",reason);
+            logger.info("contact your admins to unlock your account");
+            logger.info("------------------------");
+        }
+        catch (Exception e){
+            logger.error("failed to send account locked email to: ",email,e);
+            throw new RuntimeException("failed to send account locked email",e);
+        }
+    }
+
+    public void sendRoleChangeEmail(String email,String currentRole,String newRole){
+        try {
+            logger.info("----Role changing info----");
+            logger.info("To: {}",email);
+            logger.info("subject: your role has been changed");
+            logger.info("your role has benn changed from {} to {}",currentRole,newRole);
+            logger.info("--------------------------------");
+        }
+        catch (Exception e){
+            logger.info("failed to send role change email to: ",email,e);
+            throw new RuntimeException("failed to send role changed email",e);
+        }
     }
 }
